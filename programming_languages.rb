45494e2c2e1ef0941272bbe347dd6d5876f56bfd
Mary  Beliveau <mary.e.beliveau@gmail.com>
2018-08-24 16:31:40 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -32,18 +32,16 @@ languages = {
 def reformat_languages(languages)
   new_hash = {}
   languages.each do |style, language_data|
-    style = style.to_s
-    style = style.split
     language_data.each do |language, type_data|
       type_data.each do |type_name, type|
-        #if new_hash.keys.include? language
-         #   puts "messing with #{language} before adding to new hash"
-          #  repeat_language = language
-           # new_hash[repeat_language][:style] << style
-            #repeat_style_list = new_hash[repeat_language][:style]
-            #repeat_style_list = repeat_style_list.flatten!
-            #new_hash[language] = {:type => type, :style => #repeat_style_list}
-        #else
+        if new_hash.keys.include? language
+            puts "messing with #{language} before adding to new hash"
+            repeat_language = language
+            new_hash[repeat_language][:style] << style
+            repeat_style_list = new_hash[repeat_language][:style]
+            repeat_style_list = repeat_style_list.flatten!
+            new_hash[language] = {:type => type, :style => repeat_style_list}
+        else
           puts "adding #{language} to new hash"
           new_hash[language] = {:type => type, :style => style}
         #end     

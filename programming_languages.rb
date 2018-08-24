63b780089d62487f1ac64cc55d0a92af42cb7021
Mary  Beliveau <mary.e.beliveau@gmail.com>
2018-08-24 15:41:57 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -33,6 +33,7 @@ def reformat_languages(languages)
   new_hash = {}
   languages.each do |style, language_data|
     language_data.each do |language, type|
-      new_hash[language] 
-  
+      puts type
+    end
+  end
 end
